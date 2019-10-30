0460fb6e6047fbbb59aa74ffd41da14e4a2b616d
Lars Nielsen <lnc13.lars@gmail.com>
2019-10-30 14:45:49 +0100

    fixing bug in un hashed last string

--- src/harpocrates/hashing.cpp
+++ src/harpocrates/hashing.cpp
@@ -49,7 +49,7 @@ namespace hashing
         SHA_CTX shactx;
 
         SHA1_Init(&shactx);
-        SHA1_Update(&shactx, data.data(), data.size() - 1);
+        SHA1_Update(&shactx, data.data(), data.size());
         SHA1_Final(digest, &shactx);
 
         hash = std::vector<uint8_t>(SHA_DIGEST_LENGTH);
@@ -75,7 +75,7 @@ namespace hashing
         SHA256_CTX shactx;
     
         SHA256_Init(&shactx);
-        SHA256_Update(&shactx, data.data(), data.size() - 1);
+        SHA256_Update(&shactx, data.data(), data.size());
         SHA256_Final(digest, &shactx);
 
         hash = std::vector<uint8_t>(SHA256_DIGEST_LENGTH);
@@ -101,7 +101,7 @@ namespace hashing
         SHA512_CTX shactx;
 
         SHA512_Init(&shactx);
-        SHA512_Update(&shactx, data.data(), data.size() - 1);
+        SHA512_Update(&shactx, data.data(), data.size());
         SHA512_Final(digest, &shactx);
 
         hash = std::vector<uint8_t>(SHA512_DIGEST_LENGTH);
