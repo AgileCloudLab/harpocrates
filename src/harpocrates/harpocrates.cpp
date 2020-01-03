96e31c813356c3c156a349f8d100c934d523ef1f
Lars Nielsen <lnc13.lars@gmail.com>
2020-01-03 11:48:32 +0100

    [:hammer:] Identified the issue line 352 gives a segementation fault

--- src/harpocrates/harpocrates.cpp
+++ src/harpocrates/harpocrates.cpp
@@ -2,6 +2,8 @@
 #include "static_block.hpp" //We need this to ensure srand is called when the application starts
 
 #include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/modes.h>
 
 #include <string.h>
 #include <cstdint>
@@ -169,15 +171,39 @@ namespace harpocrates
             memcpy(ciphertext.data() + size_vec_size + padded_size, iv.data(), AES_BLOCK_SIZE);
         }
 
-        unsigned int num = 0;
-        unsigned char ecount[AES_BLOCK_SIZE]; 
+        // unsigned int num = 0;
+        // unsigned int counter = 0;
+        // unsigned char ecount[AES_BLOCK_SIZE]; 
         //Do the actual encryption
         // TODO:         AES_ctr128_encrypt(indata, outdata, bytes_read, &key, state.ivec, state.ecount, &state.num);
         // https://www.gurutechnologies.net/blog/aes-ctr-encryption-in-c/
-        AES_ctr128_encrypt(data.data(), ciphertext.data() + size_vec_size, (unsigned long) data.size(), &encryption_key, iv.data(), ecount, &num); 
-//        AES_ctr128_encrypt(data.data(), ciphertext.data() + , padded_size, &encryption_key, iv.data(), ecount, &count);
+//        AES_ctr128_encrypt(data.data(), ciphertext.data() + size_vec_size, (unsigned long) data.size(), &encryption_key, iv.data(), counter, &num, AES_ENCRYPT);
+        /// https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
+        EVP_CIPHER_CTX* ctx;
+        if (!(ctx = EVP_CIPHER_CTX_new()))
+        {
+            // TODO: Throw exception
+        }
 
-        //Save result
+        if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, ukey, iv.data()))
+        {
+            // TODO: Throw Exception
+        }
+
+        int length;
+        if (1 != EVP_EncryptUpdate(ctx, ciphertext.data(), &length, data.data(), data.size()))
+        {
+            // TODO: Throw Exception
+        }
+
+        // ciphertext.data() + length
+        if (1 != EVP_EncryptFinal_ex(ctx, ciphertext.data(), &length))
+        {
+            // TODO: Throw execption
+        }
+
+        EVP_CIPHER_CTX_free(ctx);
+ 
         data = ciphertext;
     }
 
@@ -272,6 +298,8 @@ namespace harpocrates
 	AES_KEY decrypt_key;
         AES_set_decrypt_key(ukey, HARPOCRATES_AES_KEY_SIZE * 8, &decrypt_key); //key size in bits rather than bytes
 
+
+
 	//Extract the metadata from data
 	//Format: {SIZE_OF_PADDING(1B) | PADDED_CIPHERTEXT | IV(OPTIONAL 16B)}
 	std::vector<uint8_t>::const_iterator cipher_start = data.begin() + 1;
@@ -302,16 +330,38 @@ namespace harpocrates
             iv = std::vector<uint8_t>(AES_BLOCK_SIZE, 0);             
         }
 
-        size_t cleartext_size = cipher.size() - data[0];
-        std::vector<uint8_t> decrypted(cipher.size());
+//        size_t cleartext_size = cipher.size() - data[0];
+        // std::vector<uint8_t> decrypted(cipher.size());
+        std::vector<uint8_t> decrypted;
 
-        unsigned int count = 0; 
-        // Do the actual decryption
-	AES_ctr128_encrypt(cipher.data(), decrypted.data(), cipher.size(), &decrypt_key,iv.data(), AES_DECRYPT, &count);
 
-	//Remove any potential padding
-	decrypted.resize(cleartext_size);
-        data = decrypted; 
+//         // https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
+         EVP_CIPHER_CTX* ctx;
+
+        if(!(ctx = EVP_CIPHER_CTX_new()))
+        {
+            // TODO: Throw exception 
+        }
+
+        if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, ukey, iv.data()))
+        {
+            // TODO: Throw Exceptions 
+        }
+
+        int length = 0; 
+        if (1 != EVP_DecryptUpdate(ctx, decrypted.data(), &length, cipher.data(), cipher.size()))
+        {
+            // TODO: Throw Exceptions 
+        }
+
+        if (1 != EVP_DecryptFinal_ex(ctx, decrypted.data() + length, &length))
+        {
+            // TODO: Throw excpetion
+        }
+
+         EVP_CIPHER_CTX_free(ctx);
+        
+         data = decrypted; 
     }
 }
 
