AgileCloudLab/harpocrates

commit 8153d083898ad2345c7446e651b5dce07e6e2a87

testing

src/harpocrates/hashing.cpp
@@ -1,5 +1,11 @@
 #include "hashing.hpp"
+
 #include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
+
+#include <cstring>
+
 namespace harpocrates
 {
 namespace hashing
@@ -35,6 +41,8 @@ namespace vectors
         case hash_type::SHA512:
             sha512_hash(data, hash);
             break;
+        case hash_type::HMAC:
+            hmac_hash(data, hash);
         default:
             sha1_hash(data, hash);
             break;            
@@ -90,7 +98,38 @@ namespace vectors
         {
             hash.at(i) = (uint8_t) digest[i]; 
         }
-    }    
+    }
+
+    void hmac_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash, bool empty_key)
+    {
+        std::vector<uint8_t> temp_data(data.size() + 1);
+        temp_data.at(0) = 0x01;
+        std::memcpy(temp_data.data()+1, data.data(), data.size());
+
+        std::vector<uint8_t> key;
+
+        if (!empty_key)
+        {
+            sha1_hash(data, key);
+        }
+        
+        //unsigned int length = SHA_DIGEST_LENGTH;
+        //hash = std::vector<uint8_t>(SHA_DIGEST_LENGTH);
+
+        uint8_t* temp;
+
+        temp =  (uint8_t*) ::HMAC(EVP_sha1(), key.data(), key.size(),
+                                  (unsigned char*)temp_data.data(), temp_data.size(), NULL, NULL);
+        hash = std::vector<uint8_t>(SHA_DIGEST_LENGTH);
+        memcpy(hash.data(), temp, SHA_DIGEST_LENGTH);
+        // HMAC_CTX hmacctx;
+        // HMAC_CTX_init(&hmacctx);
+
+        // HMAC_Init_ex(&hmacctx, (unsigned char*) key.data(), key.size(), EVP_sha1(), NULL);
+        // HMAC_Update(&hmacctx, (unsigned char*) temp_data.data(), data.size());
+        // HMAC_Final(&hmacctx, (unsigned char*) hash.data(), &length);
+        // HMAC_CTX_cleanup(&hmacctx);
+    }
 
 }
     

src/harpocrates/harpocrates.cpp
@@ -110,6 +110,76 @@ namespace harpocrates
 	data = ciphertext;
     }
 
+    // void encrypt_ctr(const std::string& key, std::vector<uint8_t>& data, bool random_iv)
+    // {
+    //     if (key.size() < HARPOCRATES_AES_KEY_SIZE)
+    //     {
+    //         throw std::runtime_error("Provided AES key is too short! By default key must be 16 bytes long");
+    //     }
+
+    //    	// Set up the key, convert it to the format required by OpenSSL
+    //     unsigned char ukey[HARPOCRATES_AES_KEY_SIZE];        
+    //     for (uint32_t i = 0; i < HARPOCRATES_AES_KEY_SIZE; ++i)
+    //     {
+    //         ukey[i] = (unsigned char) key[i];
+    //     }
+    //     AES_KEY encryption_key;
+    //     AES_set_encrypt_key(ukey, HARPOCRATES_AES_KEY_SIZE * 8, &encryption_key); //key size in bits rather than bytes
+
+    //     // If necessary, pad the cleartext to have a size that is a multiple of AES_BLOCK_SIZE
+    //     size_t cleartext_size = data.size();
+    //     size_t padded_size = cleartext_size;
+    //     uint8_t padding  = AES_BLOCK_SIZE - cleartext_size % AES_BLOCK_SIZE;
+	
+    //     if(padding == AES_BLOCK_SIZE)
+    //     {
+    //         padding = 0;
+    //     }
+	    
+    //     if(padding != 0)
+    //     {
+    //         padded_size += padding;
+    //         data.resize(padded_size);
+    //     }
+
+    //     size_t ciphertext_size = padded_size;
+	
+    //     // Generate the initialization vector, either randomly or by chosing an all-0 vector
+    //     std::vector<uint8_t> iv;        
+    //     if (random_iv)
+    //     {
+    //         iv = generate_iv(AES_BLOCK_SIZE);
+    //         ciphertext_size += AES_BLOCK_SIZE;
+    //     }
+    //     else
+    //     {
+    //         iv = std::vector<uint8_t>(AES_BLOCK_SIZE, 0);
+    //     }
+
+    //     //Generate a vector representing the original cleartext_size
+    //     const size_t size_vec_size = 1;// Extra byte to represent the amount of padding
+    //     ciphertext_size += 1;
+
+    //     //Create the vector that will hold the ciphertext and emplace the metadata to its end
+    //     //Format: {SIZE_OF_PADDING(1B) | PADDED_CIPHERTEXT | IV(OPTIONAL 16B)} 
+    //     std::vector<uint8_t> ciphertext(ciphertext_size);       
+    //     ciphertext[0] = padding;
+    //     if(random_iv)
+    //     {
+    //         memcpy(ciphertext.data() + size_vec_size + padded_size, iv.data(), AES_BLOCK_SIZE);
+    //     }
+		
+    //     //Do the actual encryption
+    //     // TODO:         AES_ctr128_encrypt(indata, outdata, bytes_read, &key, state.ivec, state.ecount, &state.num);
+    //     // https://www.gurutechnologies.net/blog/aes-ctr-encryption-in-c/
+    //     AES_ctr128_encrypt(data.data(), ciphertext.data() + size_vec_size, padded_size, &encryption_key, iv.data(), AES_ENCRYPT);
+
+    //     //Save result
+    //     data = ciphertext;
+    // }
+
+    
+    
     /* Decrypts the data based on the provided key using AES-CBC-128.
     * If the key is longer than 16Bytes, no problem we cut it off
     * The input vector is overwritten to contain the result of the decryption. The expected input format:

src/harpocrates/harpocrates.hpp
@@ -30,6 +30,8 @@ namespace harpocrates
     */
     void encrypt(const std::string& key, std::vector<uint8_t>& data, bool random_iv=false);
 
+    // void encrypt_ctr(const std::string& key, std::vector<uint8_t>& data, bool random_iv=false);    
+
     /* Decrypts the data based on the provided key using AES-CBC-128.
     * If the key is longer than 16Bytes, no problem we cut it off
     * The input vector is overwritten to contain the result of the decryption. The expected input format:
@@ -43,6 +45,8 @@ namespace harpocrates
     * @paramrandom_iv determines if a random IV was used for encryption. If set to false we will use an all 0 IV
     */
     void decrypt(const std::string& key, std::vector<uint8_t>& data, bool random_iv=false);
+
+    void decrypt_ctr(const std::string& key, std::vector<uint8_t>& data, bool random_iv=false);    
 }
 #endif /* HARPOCRATES_H */ 
 

src/harpocrates/hashing.hpp
@@ -12,7 +12,8 @@ enum hash_type
 {
     SHA1,
     SHA256,
-    SHA512                
+    SHA512,
+    HMAC,
 };
 
     /// Returns length of digest for specified hash function
@@ -43,6 +44,8 @@ namespace vectors
     /// @param data the data chunk the fingerprint will be constructed for
     /// @param hash the sink for the hash        
     void sha512_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash);
+
+    void hmac_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash, bool empty_key=true);
 }
 
 namespace pointers

test/test_hashing/test_hashing.cpp
@@ -12,6 +12,7 @@
 #include <utility>
 #include <sstream>
 
+
 void print_hex(const std::vector<uint8_t>& data)
 {
     std::stringstream ss;
@@ -20,7 +21,7 @@ void print_hex(const std::vector<uint8_t>& data)
         ss << std::hex << (int) elm; 
     }
 
-    std::cout << ss.str() << std::endl; 
+    std::cout <<  ss.str() << std::endl; 
 }
 
 std::vector<uint8_t> generate_data(size_t size)
@@ -161,6 +162,48 @@ TEST(test_hashing, test_sha512_hashing_4kb)
 
     EXPECT_EQ(hash1, hash2);
     EXPECT_NE(hash1, hash3);    
+}
+
+TEST(test_hashing, test_hmac_vector_hashing_1kb)
+{
+    std::vector<uint8_t> data = generate_data(1024);
+
+    std::vector<uint8_t> hash1;
+    std::vector<uint8_t> hash2;
+    harpocrates::hashing::vectors::hmac_hash(data, hash1);
+    EXPECT_EQ(hash1.size(), (size_t)20);
+
+    harpocrates::hashing::vectors::hmac_hash(data, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)20);
+
+    EXPECT_EQ(hash1, hash2);
+
+    std::vector<uint8_t> data2 = generate_data(1024);
+    harpocrates::hashing::vectors::hmac_hash(data2, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)20);
+
+    EXPECT_NE(hash1, hash2);
+}
+
+TEST(test_hashing, test_hmac_vector_hashing_4kb)
+{
+    std::vector<uint8_t> data = generate_data(4096);
+
+    std::vector<uint8_t> hash1;
+    std::vector<uint8_t> hash2;
+    harpocrates::hashing::vectors::hmac_hash(data, hash1);
+    EXPECT_EQ(hash1.size(), (size_t)20);
+
+    harpocrates::hashing::vectors::hmac_hash(data, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)20);
+
+    EXPECT_EQ(hash1, hash2);
+
+    std::vector<uint8_t> data2 = generate_data(4096);
+    harpocrates::hashing::vectors::hmac_hash(data2, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)20);
+
+    EXPECT_NE(hash1, hash2);
 }
 
 int main(int argc, char **argv) {

commit e500a7e0ca39d763fd8169bc7a0a38fde86af08a

Add missing break statement in hash method for HMAC hash.
The switch statement was missing a break statement which causes the HMAC to be overwritten by a SHA-1 hash. By adding the break statement, the HMAC no longer gets overwritten.

Signed-off-by: ChrisG55 <christian_g_55@msn.com>
Reported-by: ChrisG55 <christian_g_55@msn.com>
Fixes: 8153d083898ad2345c7446e651b5dce07e6e2a87

src/harpocrates/hashing.cpp
@@ -43,6 +43,7 @@ namespace vectors
             break;
         case hash_type::HMAC:
             hmac_hash(data, hash);
+	    break;
         default:
             sha1_hash(data, hash);
             break;            