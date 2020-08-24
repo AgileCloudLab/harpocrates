AgileCloudLab/harpocrates

commit 4e28e8ad48d18a3ca0c09bb5eaca39654398d720

Add CRC-32 support
Signed-off-by: ChrisG55 <christian_g_55@msn.com>

src/harpocrates/hashing.cpp
@@ -4,7 +4,11 @@
 #include <openssl/hmac.h>
 #include <openssl/evp.h>
 
+#include <algorithm>
+#include <array>
+#include <cstdint>
 #include <cstring>
+#include <numeric>
 
 namespace harpocrates
 {
@@ -21,6 +25,8 @@ namespace hashing
 	    return SHA256_DIGEST_LENGTH;
         case hash_type::SHA512:
             return SHA512_DIGEST_LENGTH;
+	case hash_type::CRC32:
+            return CRC_DIGEST_LENGTH;
         default:
             return SHA_DIGEST_LENGTH;
         }
@@ -43,6 +49,10 @@ namespace vectors
             break;
         case hash_type::HMAC:
             hmac_hash(data, hash);
+	    break;
+	case hash_type::CRC32:
+            crc32_hash(data, hash);
+	    break;
         default:
             sha1_hash(data, hash);
             break;            
@@ -125,6 +135,40 @@ namespace vectors
         memcpy(hash.data(), temp, SHA_DIGEST_LENGTH);
     }
 
+    std::array<std::uint_fast32_t, 256> generate_crc_lookup_table() noexcept
+    {
+        auto const reversed_polynomial = std::uint_fast32_t{0xEDB88320uL};
+
+        struct byte_checksum
+	{
+	    std::uint_fast32_t operator()() noexcept
+	    {
+                auto checksum = static_cast<std::uint_fast32_t>(n++);
+      
+                for (auto i = 0; i < 8; ++i)
+                    checksum = (checksum >> 1) ^ ((checksum & 0x1u) ? reversed_polynomial : 0);
+
+                return checksum;
+            }
+
+            unsigned n = 0;
+        };
+
+	auto table = std::array<std::uint_fast32_t, 256>{};
+	std::generate(table.begin(), table.end(), byte_checksum{});
+
+	return table;
+    }
+  
+    void crc32_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash)
+    {
+        static auto const table = generate_crc_lookup_table();
+
+        std::uint_fast32_t temp = std::uint_fast32_t{0xFFFFFFFFuL} & ~std::accumulate(data.cbegin(), data.cend(), ~std::uint_fast32_t{0} & std::uint_fast32_t{0xFFFFFFFFuL}, [](std::uint_fast32_t checksum, std::uint_fast32_t value){return table[(checksum ^ value) & 0xFFu] ^ (checksum >> 8);});
+        hash.reserve(CRC_DIGEST_LENGTH);
+        for (int i = CRC_DIGEST_LENGTH - 1; i >= 0; --i)
+	    hash.push_back(*(static_cast<uint8_t *>(static_cast<void *>(&temp)) + i));
+    }
 }
     
 namespace pointers

src/harpocrates/hashing.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cstdint>
 
+#define CRC_DIGEST_LENGTH 4
+
 namespace harpocrates
 {
 namespace hashing
@@ -14,6 +16,7 @@ enum hash_type
     SHA256,
     SHA512,
     HMAC,
+    CRC32,
 };
 
     /// Returns length of digest for specified hash function
@@ -46,6 +49,11 @@ namespace vectors
     void sha512_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash);
 
     void hmac_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash, bool empty_key=true);
+
+    /// Creates a CRC-32 hash finger print for a chunk
+    /// @param data the data chunk the fingerprint will be constructed for
+    /// @param hash the sink for the hash
+    void crc32_hash(const std::vector<uint8_t>& data, std::vector<uint8_t>& hash);
 }
 
 namespace pointers

test/test_hashing/test_hashing.cpp
@@ -249,6 +249,48 @@ TEST(test_hashing, test_hmac_vector_hashing_4kb_none_empty_key)
     EXPECT_NE(hash1, hash2);
 }
 
+TEST(test_hashing, test_crc_vector_hashing_1kb)
+{
+    std::vector<uint8_t> data = generate_data(1024);
+
+    std::vector<uint8_t> hash1;
+    std::vector<uint8_t> hash2;
+    harpocrates::hashing::vectors::crc32_hash(data, hash1);
+    EXPECT_EQ(hash1.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    harpocrates::hashing::vectors::crc32_hash(data, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    EXPECT_EQ(hash1, hash2);
+
+    std::vector<uint8_t> data2 = generate_data(1024);
+    harpocrates::hashing::vectors::crc32_hash(data2, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    EXPECT_NE(hash1, hash2);
+}
+
+TEST(test_hashing, test_crc_vector_hashing_4kb)
+{
+    std::vector<uint8_t> data = generate_data(4096);
+
+    std::vector<uint8_t> hash1;
+    std::vector<uint8_t> hash2;
+    harpocrates::hashing::vectors::crc32_hash(data, hash1);
+    EXPECT_EQ(hash1.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    harpocrates::hashing::vectors::crc32_hash(data, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    EXPECT_EQ(hash1, hash2);
+
+    std::vector<uint8_t> data2 = generate_data(4096);
+    harpocrates::hashing::vectors::crc32_hash(data2, hash2);
+    EXPECT_EQ(hash2.size(), (size_t)CRC_DIGEST_LENGTH);
+
+    EXPECT_NE(hash1, hash2);
+}
+
 int main(int argc, char **argv) {
 
     srand(static_cast<uint32_t>(time(0)));

commit 286a50189c5e5f2a6589f03c9f74ad30cea8e2c2

Clear vectors when using CRC32 hash
If a vector is reused with a CRC32 hash, then the new hash gets appended to the old hash. This is against the intuition of a user, who expects a new hash where the old value is overwritten. This commit fixes the issue by clearing the hash vector before appending the new hash bytes.

Reported-by: Lars Nielsen <lani@eng.au.dk>
Fixes: 4e28e8ad48d18a3ca0c09bb5eaca39654398d720
Tested-by: ChrisG55 <christian_g_55@msn.com>
Signed-off-by: ChrisG55 <christian_g_55@msn.com>

src/harpocrates/hashing.cpp
@@ -165,6 +165,7 @@ namespace vectors
         static auto const table = generate_crc_lookup_table();
 
         std::uint_fast32_t temp = std::uint_fast32_t{0xFFFFFFFFuL} & ~std::accumulate(data.cbegin(), data.cend(), ~std::uint_fast32_t{0} & std::uint_fast32_t{0xFFFFFFFFuL}, [](std::uint_fast32_t checksum, std::uint_fast32_t value){return table[(checksum ^ value) & 0xFFu] ^ (checksum >> 8);});
+        hash.clear();
         hash.reserve(CRC_DIGEST_LENGTH);
         for (int i = CRC_DIGEST_LENGTH - 1; i >= 0; --i)
 	    hash.push_back(*(static_cast<uint8_t *>(static_cast<void *>(&temp)) + i));